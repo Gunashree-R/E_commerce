4a8d9cf18fe4823861d95aaf53f63798871f3311
Gunashree-R <gunashree1811@gmail.com>
2024-08-13 00:11:14 +0530

    feat : style file

---
+++ collection.js
@@ -0,0 +1,22 @@
+var productcont = document.getElementById("n");
+var search = document.getElementById("search");
+var productlist = productcont.querySelectorAll("div");
+search.addEventListener("keyup",function(){
+    var enteredvalue = event.target.value.toUpperCase();
+    for(i=0;i<productlist.length;i=i+1)
+    {
+        var productname = productlist[i].querySelector("p").textContent
+        if(productname.toUpperCase().indexOf(enteredvalue)<0)
+        {
+            productlist[i].style.display="none"
+        }
+        else{
+            productlist[i].style.display="block"
+        }
+    }
+})
+
+function myfunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
---
+++ script.js
@@ -0,0 +1,16 @@
+function myfunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+var sidenav = document.querySelector(".side-navbar");
+
+function showNavbar() {
+    sidenav.style.display = "block";
+}
+function closeNavbar() {
+    sidenav.style.display = "none";
+}
+
+
+
